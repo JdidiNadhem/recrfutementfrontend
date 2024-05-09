7ec7e6f2701656723450aee8466cab1d444ff59e
jdidiNadhem <jedidinadhem@gmail.com>
2024-05-10 01:54:47 +0200

    add job-description

---
+++ src/Components/home/JobCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const JobCard = ({ company, image, title, description }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4 mb-4 flex items-center">
+      <img src={image} alt={company} className="w-16 h-16 rounded-5 mr-4" />
+      <div>
+        <Link to="job-description">
+          <h3 className="text-lg font-semibold">{title}</h3>
+        </Link>
+        <p className="text-gray-600">{company}</p>
+        <p className="text-sm text-gray-700 mt-2">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
